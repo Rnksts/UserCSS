b500e5c213904745d3136f32b236b4b77064796b
Manu <20311303+Rnksts@users.noreply.github.com>
2020-05-01 19:11:25 -0400

    Update bitly.user.js

--- bitly.user.js
+++ bitly.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name          Bitly Clean
 // @namespace     Rnksts
-// @version       1.0.0
+// @version       1.0.1
 // @homepage      https://github.com/Rnksts/UserCSS
 // @description   Remove bitly clutter, dark/light theme
 // @license       CC-BY-SA-4.0
@@ -9,99 +9,89 @@
 // @include       https://bitly.com/
 // @run-at        document-start
 // ==/UserScript==
-(function() {var css = [`
-:root {
-    --dark: #151515;
-    --dark2: #1b1b1b;
-    --dark3: #404040;
-    --text: #ccc;
-    --text2: #fff;
-    --accent: #EE6123;
-}
-.container.container-pod.container-pod-tall.overview,
-.stage-pod.stage-pod-light,
-.page-shorten-brand-tools-product,
-.page-about-values-navigation,
-.page-about-value,
-.bottom-cta,
-.footer,
-.three-columns,
-.scroll-for-more,
-.header,
-.gallery,
-.content-divider,
-div.cell h3.header-m,
-.promo-customize,
-#page-home-link-shortener,
-#page-about-values,
-#promo-container,
-.brand-subhead,
-.page-home-trust-quote,
-.frame,
-.stage-pod,
-.page-home-trust-quote,
-h1.page-title,
-.join-bitly,
-a.button.button-primary,
-div.promo-container,
-nav.navigation,
-.hero-wrap,
-.mobile-hero-wrap img,
-#shorten_actions .anon_history li + li:before,
-#form_container > p, .sitebanner--spacer,.sitebanner--container,
-a[href="https://bitly.com/pages/features/branded-links"],
-a[href="https://bitly.com/pages/pricing"] {
-    display: none !important
-}
-.link-container,
-#form_container {
-    width: 90%;
-}
-.hero-content {
-    max-height: 300px;
-    background-image: none !important;
-}
-.mobile-hero-wrap {
-    max-height: 300px;
-}
-body,
-.hero-content,
-.full-width-content,
-.anon_history {
-    background-color: var(--dark) !important;
-}
-li.link {
-    background-color: var(--dark2) !important;
-    color: var(--text) !important;
-}
-.short-link a,
-#form_container #shortened_url {
-    color: var(--accent) !important;
-}
-.button,
-#form_container .shorten-button {
-    background-color: var(--dark2);
-    color: var(--text);
-    border-color: var(--accent);
-}
-.button:hover,
-#form_container .shorten-button:hover {
-    background-color: var(--accent);
-    color: var(--text2);
-    border-color: var(--accent);
-}
-#form_container input[type="text"] {
-    background: var(--dark2);
-    border: 1px solid var(--dark3);
-    color: var(--text);
-}
-#shorten_actions .anon_history li {
-    border: 1px solid var(--dark3) !important;
-    border-radius: 8px;
-}
-`];
-var node = document.createElement("style");
-node.type = "text/css";
-node.appendChild(document.createTextNode(css));
-document.documentElement.appendChild(node);
+(function () {
+	const dark = true;
+
+	let scheme =
+		dark === true
+			? '--bg: #0a0a0a;\n			--bg2: #282828;\n			--text: #ccc;'
+			: '--bg: #fff;\n			--bg2: #eee;\n			--text: #000;';
+	let css = `
+		:root {
+			${scheme}
+			--text2: #fff;
+			--accent: #ee6123;
+		}
+		.header,
+		.hero-content img,
+		.large-9,
+		.text-content:not(.url-shorten-row),
+		#unAuthShortenForm + p,
+		.anon_history li + li:before,
+		.text-center,
+		.content-divider,
+		.bottom-cta,
+		footer,
+		.wow-modal-window {
+			display: none !important;
+		}
+		.mobile-hero-wrap {
+			max-height: 200px !important;
+		}
+		body,
+		.hero-content,
+		.full-width-content,
+		.anon_history {
+			background: var(--bg) !important;
+		}
+		.link {
+			background-color: var(--bg2) !important;
+			color: var(--text) !important;
+		}
+		.short-link a,
+		#shortened_url {
+			color: var(--accent) !important;
+		}
+		.short-link a:hover {
+			text-decoration: underline !important;
+		}
+		.button {
+			background-color: var(--bg2) !important;
+			color: var(--text) !important;
+			border-color: var(--accent) !important;
+		}
+		.button:hover {
+			background-color: var(--accent) !important;
+			color: var(--text2) !important;
+			border-color: var(--accent) !important;
+		}
+		.shorten-input {
+			background: var(--bg2) !important;
+			border: 1px solid var(--bg2) !important;
+			color: var(--text) !important;
+		}
+		#shorten_url:hover,
+		#shortened_url:hover {
+			border: 1px solid var(--accent) !important;
+		}
+		#shorten_url:focus,
+		#shortened_url:focus,
+		.button:focus {
+			border: 1px solid var(--accent) !important;
+			-Webkit-box-shadow: none !important;
+			-moz-box-shadow: none !important;
+			box-shadow: none !important;
+		}
+		.button-secondary.copied {
+			color: var(--accent) !important;
+			background: var(--accent) !important;
+		}
+		.anon_history li {
+			border-radius: 8px !important;
+		}
+	`;
+	let node = document.createElement('style');
+	node.type = 'text/css';
+	node.appendChild(document.createTextNode(css));
+	document.documentElement.appendChild(node);
 })();
